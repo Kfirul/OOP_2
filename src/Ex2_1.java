6dd40bd79533c13a3d160f2482484157823caa37
Kfir Zilbernagel <kfirul14@gmail.com>
2023-01-01 16:25:33 +0200

    init

---
+++ src/Ex2_1.java
@@ -0,0 +1,11 @@
+public class Ex2_1 {
+
+    public static String[] createTextFiles(int n, int seed, int bound){
+        String[] files=new String[n];
+        //code
+        return files;
+    }
+    public static int getNumOfLines(String[] fileNames){
+
+    }
+}
